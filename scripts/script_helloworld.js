83b26ec5dda74e49fc640c33b89eac9f8eb68810
Akhdan Robbani <danakhdan12@gmail.com>
2023-06-10 22:45:35 +0700

    learn String and escape sequence

--- scripts/script_helloworld.js
+++
@@ -1,3 +0,0 @@
-document.writeln("<br>from external js <br> hello world");
-document.writeln("<br>");
-document.writeln("Name : " + "Akhdan Robbani");
---
+++ scripts/string_datatype.js
@@ -0,0 +1,12 @@
+document.writeln("<br>from external js <br> hello world");
+document.writeln("<br>");
+
+// bisa juga menambahkan 2 string menggunakan operator +
+document.writeln("Name : " + "Akhdan Robbani");
+document.writeln("<br><br>");
+document.writeln("<textarea cols ='100' rows = '10'>");
+document.write("Akhdan \t Robbani \n");
+document.write("home\\Learn\\JavaScript \n");
+document.write("\"Mahasiswa dari Polije\"\n");
+document.write("\'Belajar JavaScript\'");
+document.writeln("</textarea>");
